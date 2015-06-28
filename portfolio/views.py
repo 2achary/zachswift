c6e7583058ab6d91900cc1677c0ef9eda68f97de
Zach <cras.zswift@gmail.com>
2015-06-28 11:19:58 -0500

    fix typo

--- portfolio/views.py
+++ portfolio/views.py
@@ -27,17 +27,17 @@ def projects(request):
 
 def sms(fromnum, body):
 
-	account_sid = "AC5d7ec2b523b18b015df1e251394e0c62"
-	auth_token  = "c56a669bfcbe7c2ac8e27f0ff4268950"
-	client = TwilioRestClient(account_sid, auth_token)
-	if "gym" in body.lower():
-		msg = "13579#"
-		message = client.messages.create(body=msg, to=fromnum from_='+16155490818')
-		return message.sid
-	else:
-		msg = "Didn't recognize that command."
-		message = client.messages.create(body=msg, to=fromnum from_='+16155490818')
-		return message.sid
+    account_sid = "AC5d7ec2b523b18b015df1e251394e0c62"
+    auth_token  = "c56a669bfcbe7c2ac8e27f0ff4268950"
+    client = TwilioRestClient(account_sid, auth_token)
+    if "gym" in body.lower():
+        msg = "13579#"
+        message = client.messages.create(body=msg, to=fromnum, from_='+16155490818')
+        return message.sid
+    else:
+        msg = "Didn't recognize that command."
+        message = client.messages.create(body=msg, to=fromnum, from_='+16155490818')
+        return message.sid
 
 
 
