7a7e7d1a5f7ed305dee59d61726981c30390e382
Zach <cras.zswift@gmail.com>
2016-04-09 20:00:46 -0500

    redo branch

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.paginator import Paginator
-from twilio.rest import TwilioRestClient
-from django_twilio.decorators import twilio_view
-from twilio.twiml import Response
 from .models import Profile, Python, Web, WorkHistory, Contact, Education, General, OS, Deployment, Testing, Blog
 
 def home(request):
@@ -85,33 +82,3 @@ def spoiler(request):
 def projects(request):
     return render(request, 'projects.html')
 
-@twilio_view
-def sms(request):
-
-    body = request.GET.get('Body', '')
-    if "gym" in body.lower():
-        msg = "13579#"
-        r = Response()
-        r.message(msg)
-        return r
-    else:
-        msg = "Didn't recognize that command."
-        r = Response()
-        r.message(msg)
-        return r
-    #
-    # fromnum = "+" + request.GET.get("From")[2:]
-    # account_sid = "AC5d7ec2b523b18b015df1e251394e0c62"
-    # auth_token  = "c56a669bfcbe7c2ac8e27f0ff4268950"
-    # client = TwilioRestClient(account_sid, auth_token)
-    # if "gym" in request['Body'].lower():
-    #     msg = "13579#"
-    #     message = client.messages.create(body=msg, to=fromnum, from_='+16155490818')
-    #     return message.sid
-    # else:
-    #     msg = "Didn't recognize that command."
-    #     message = client.messages.create(body=msg, to=fromnum, from_='+16155490818')
-    #     return message.sid
-    #
-    #
-    #
--- zachswift/settings.py
+++ zachswift/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
-    'django_twilio',
 )
 
 MIDDLEWARE_CLASSES = (
